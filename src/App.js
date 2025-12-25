35ebb66418452076f8b8caad24594cae30b6c4b9
Ubuntu <ubuntu@ip-172-31-0-173.eu-central-1.compute.internal>
2025-12-25 08:55:05 +0000

    upload code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React Frontend!</h1>
+      <p>This is a simple Nginx-served React app.</p>
+    </div>
+  );
+}
+
+export default App;
+
