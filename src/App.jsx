0f805615f18a66394bb44e410ac631861e2ee770
qshahrour <109384983+qshahrour@users.noreply.github.com>
2025-12-29 03:34:03 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from Vite + React + Kubernetes!</h1>
+      <p>This is a minimal React app ready for Docker and Ingress.</p>
+    </div>
+  );
+}
