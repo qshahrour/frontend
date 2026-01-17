bc2a99bc1a79c907ae33c26324a8eb44c710e4a1
qshahrour <109384983+qshahrour@users.noreply.github.com>
2026-01-17 18:11:10 +0300

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,7 +9,7 @@ function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://pyapi.dev.svc.cluster.local/login", {
+    const res = await fetch("http://pyapi.dev.svc.cluster.local:5000/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
