eb44bb9ccf8da2863da59a8e7fb71fd502ffbb9d
qshahrour <109384983+qshahrour@users.noreply.github.com>
2026-01-17 12:29:19 +0300

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { API_URL } from "./config";
 
 function Login({ onLogin }) {
   const [email, setEmail] = useState("");
@@ -8,7 +9,7 @@ function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://pyapi.dev.svc.cluster.local:5000/login", {
+    const res = await fetch(`${API_URL}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
