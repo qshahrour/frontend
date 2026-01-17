cf4ffbc2a81a5f79d579efd5db07a18d4fb2e50e
qshahrour <109384983+qshahrour@users.noreply.github.com>
2026-01-17 13:57:19 +0300

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,7 +9,7 @@ function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`${API_URL}:5000/login`, {
+    const res = await fetch("${API_URL}:5000/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
