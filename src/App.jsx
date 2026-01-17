fc82574044827866bf0ac4ff776ad7723548253b
qshahrour <109384983+qshahrour@users.noreply.github.com>
2026-01-17 06:28:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Login from "./Login";
 
 export default function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello from Vite + React + Kubernetes!</h1>
-      <p>This is a minimal React app ready for Docker and Ingress.</p>
+      <Login />
     </div>
   );
 }
